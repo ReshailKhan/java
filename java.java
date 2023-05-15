55ba8769d51bbe7a6f51ce22a5f3bd66a7475351
reshailkhan <reshailkhan942@gmail.com>
2023-05-15 11:14:13 +0500

    Duplicate count

--- java.java
+++ java.java
@@ -1,5 +1,7 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class java {
     public static void main(String[] args) {
@@ -9,6 +11,27 @@ public class java {
         System.out.println(dtf.format(now));
         System.out.println(now);
         //B b = new A();
+        String inputString = "Muhammad Reshail Khan";
+        Map<Character, Integer> charCountMap = new HashMap<>();
 
+        // Convert the string to lowercase (optional, to ignore case)
+        inputString = inputString.toLowerCase();
+
+        // Iterate through each character in the input string
+        for (char c : inputString.toCharArray()) {
+
+            if (charCountMap.containsKey(c))
+                charCountMap.put(c,charCountMap.get(c)+1);
+            else
+                charCountMap.put(c, 1);
+        }
+
+        // Iterate through the map entries and print the character count
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+            char c = entry.getKey();
+            int count = entry.getValue();
+            System.out.println("Character '" + c + "' appears " + count + " times.");
+
+        }
     }
 }
