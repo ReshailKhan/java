ccb175bb9713313dbed9282cb0d887f4b3893733
ReshailKhan <reshailkhan942@gmail.com>
2023-08-30 23:47:55 +0500

    up casting example

--- typecasting/UpCasting.java
+++ typecasting/UpCasting.java
@@ -3,7 +3,9 @@ package typecasting;
 public class UpCasting {
     public static void main(String[] args) {
             Animal animal = new Dog(); // Auto-upcasting
+            // Dog dog = (Dog) new Animal(); // Auto-down-casting   this code give compile time error
             animal.sound();
+            //dog.sound();
             // Calls Dog's overridden sound() method
     }
 
